ea3056a7b44336b5acb46f3dd06d87ebc5abce50
Cristian Saavedra <ccsi923@gmail.com>
2022-07-30 12:18:02 +0200

    partida creada

--- src/model/Partida.java
+++ src/model/Partida.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Partida {
 
@@ -16,16 +17,48 @@ public class Partida {
     private void initPreguntas() {
         this.preguntas = new ArrayList<>();
 
-        Pregunta firstPregunta = new Pregunta("¿La capital de francia es paris?", true, 1);
+        Pregunta firstPregunta = new Pregunta("¿La capital de Francia es Paris?", true, 1);
+        Pregunta secPregunta = new Pregunta("¿La capital de España es Berlín?", false, 1);
+        Pregunta thirdPregunta = new Pregunta("¿La capital de Alemania es Praga?", false, 1);
+        Pregunta fourthPregunta = new Pregunta("¿La capital de Venezuela es Caracas?", true, 1);
+        Pregunta fifthPregunta = new Pregunta("¿La capital de Mexico es Miami?", false, 1);
+
 
         this.preguntas.add(firstPregunta);
-
+        this.preguntas.add(secPregunta);
+        this.preguntas.add(thirdPregunta);
+        this.preguntas.add(fourthPregunta);
+        this.preguntas.add(fifthPregunta);
     }
 
     public Partida() {
         initPreguntas();
         initPlayer("Juan");
     }
+    public void jugarPartida() {
 
+        for (int i = 0; i < preguntas.size(); i++) {
+            Pregunta preguntaActual = preguntas.get(i);
+            mostrarPregunta(preguntaActual);
+            boolean respuesta = preguntaUsuario();
+            if (respuesta == preguntaActual.isTrue()) {
+                this.score = this.score + preguntaActual.getDificultad();
+                System.out.println("Has acertado, tu puntuacion se ha incrementado a: " + this.score);
+            } else {
+                System.out.println("Buen intento, has fallado");
+            }
+        }
+    }
+
+    private void mostrarPregunta(Pregunta pregunta) {
+        String afirmacion = pregunta.getAfirmacion();
+        System.out.println(afirmacion);
+    }
+
+    private boolean preguntaUsuario() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("true o false?");
+        return scanner.nextBoolean();
+    }
 
 }
